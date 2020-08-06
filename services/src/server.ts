d95cf20698ce885c3b41320c47f0f68a414bd94b
Matt Schafer <lucidsigma17@gmail.com>
2020-08-06 13:21:40 +1000

    Configure express.

--- services/src/server.ts
+++ services/src/server.ts
@@ -1,10 +1,16 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import methodOverride from "method-override";
+
+dotenv.config({
+	path: __dirname + "/../.env",
+});
 
 const app = express();
-
-app.get("/", (_request, response) => {
-	response.send("Working.");
-});
+app.use(cors());
+app.use(express.json());
+app.use(methodOverride());
 
 const port = +process.env.PORT || 8080;
 

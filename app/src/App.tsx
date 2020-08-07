29346c874110f095ce6bb4a5df73556ced32eab2
Matt Schafer <lucidsigma17@gmail.com>
2020-08-07 13:22:46 +1000

    Implement React router.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 
 import './App.css';
 
@@ -6,8 +7,10 @@ export type AppProps = { };
 
 export default (_props: AppProps) => {
 	return (
-		<div className="App">
-			<h1>Working.</h1>
-		</div>
+		<Router>
+			<div className="App">
+				
+			</div>
+		</Router>	
 	);
 };

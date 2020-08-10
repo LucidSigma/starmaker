facc4625e384c7170ca0be04d1ab75a42d4bf42a
Matt Schafer <lucidsigma17@gmail.com>
2020-08-10 11:51:33 +1000

    Add home page styling.

--- app/src/components/HomePage/HomePage.tsx
+++ app/src/components/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Button } from "react-bootstrap"
 import { Link } from "react-router-dom";
+
+import styleClasses from "./HomePage.module.scss";
 
 export type HomePageProps = { };
 
 export default (_props: HomePageProps) => {
 	return (
 		<div>
-			<Link to="/stars">Go to Stars</Link>
+			<h1 className={ styleClasses.Header }>Welcome to StarMaker!</h1>
+			<p className={ styleClasses.Subtitle }>Where you can create all sorts of stars and planets!</p>
+
+			<Link className={ styleClasses.Link } to="/stars">
+				<Button className={ styleClasses.Button }>Go to Stars</Button>
+			</Link>
 		</div>
 	);
 };
